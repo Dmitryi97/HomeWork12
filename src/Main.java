df1ba5f0a5dc12b91bfe0470d48696500812fcb2
Dmitryi <pytin97@bk.ru>
2024-02-28 15:58:37 +0300

    Выполнил домашку!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //Превая книга
+        Author author = new Author("Федор", "Достоевский");
+        Book book = new Book("Преступление и наказание", author, 1866);
+        System.out.println("Автор: " + author.getName() + " " + author.getSurname());
+        System.out.println("Произведение: " + book.getName() + " " + ", публикация " + book.getYearPublication() + " года");
+        //Вторая книга
+        Author author2 = new Author("Александр", "Дюма");
+        Book book2 = new Book("Граф Монте-Кристо", author2, 1846);
+        System.out.println("Автор: " + author2.getName() + " " + author2.getSurname());
+        System.out.println("Произведение: " + book2.getName() + " " + ", публикация " + book2.getYearPublication() + " года");
+        //Меняем год публикации
+        book2.setYearPublication(1847);
+        System.out.println("Произведение: " + book2.getName() + " " + ", публикация " + book2.getYearPublication() + " года");
     }
 }
