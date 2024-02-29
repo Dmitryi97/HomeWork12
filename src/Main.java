8ebd6dab9722b0e96d52af1771224dd93c36d33f
Dmitryi <pytin97@bk.ru>
2024-02-29 16:58:43 +0300

    Выполнил ДЗ №13!

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,22 @@ public class Main {
         //Превая книга
         Author author = new Author("Федор", "Достоевский");
         Book book = new Book("Преступление и наказание", author, 1866);
-        System.out.println("Автор: " + book.getAuthor().getName() + " " + book.getAuthor().getSurname());
-        System.out.println("Произведение: " + book.getName() + " " + ", публикация " + book.getYearPublication() + " года");
+        System.out.println(book);
+        System.out.println();
         //Вторая книга
         Author author2 = new Author("Александр", "Дюма");
         Book book2 = new Book("Граф Монте-Кристо", author2, 1846);
-        System.out.println("Автор: " + book2.getAuthor().getName() + " " + book2.getAuthor().getSurname());
-        System.out.println("Произведение: " + book2.getName() + " " + ", публикация " + book2.getYearPublication() + " года");
+        System.out.println(book2);
+        System.out.println();
         //Меняем год публикации
         book2.setYearPublication(1847);
-        System.out.println("Произведение: " + book2.getName() + " " + ", публикация " + book2.getYearPublication() + " года");
+        System.out.println(book2);
+        System.out.println();
+        //Сравним книги
+        System.out.println(book.equals(book2));
+        //Сравним авторов
+        System.out.println(author.equals(author2));
+        //сравним имена авторов
+        System.out.println(author.getName().equals(author2.getName()));
     }
 }
